e81a66d59df79795bd417fe145fc3ab2f4fdc03f
claus42 <nikolai.androidrus@gmail.com>
2017-10-31 23:09:40 +0300

    Выполнил задание 2: реализовал toString()

--- src/ru/xou/task01_cloning/Bike.java
+++ src/ru/xou/task01_cloning/Bike.java
@@ -9,9 +9,29 @@ class Wheel {
 
 public class Bike implements Cloneable{
 
+    /*
+    @Override
+    public Bike clone() {
+        try {
+             super.clone();
+            //return (Bike) super.clone();
+        }
+        catch( CloneNotSupportedException ex ) {
+            throw new InternalError();
+        }
+    }
+    */
+
     @Override
     protected Bike clone() {
             return new Bike(this.wheelCount, this.w.d);
+    }
+
+    @Override
+    public String toString(){
+        return "The bike has " + this.wheelCount + " with diameter of " +
+                this.w.d + " cm";
+
     }
 
     int wheelCount;
@@ -25,16 +45,16 @@ public class Bike implements Cloneable{
         Bike bike1 = new Bike(2, 26);
         Bike bike2 = bike1.clone();
 
+        System.out.println(bike1);
+        System.out.println(bike2);
+
         bike1.wheelCount = 1;
         bike1.w.d = 29;
         bike2.wheelCount = 4;
         bike2.w.d = 35;
 
-
-        System.out.println(bike1.wheelCount);
-        System.out.println(bike1.w.d);
-        System.out.println(bike2.wheelCount);
-        System.out.println(bike2.w.d);
+        System.out.println(bike1);
+        System.out.println(bike2);
 
     }
 }
